1ad2112a73834bfecbe4d78ebc830b5856234f5f
Von <pedrodeibarlucea@gmail.com>
2020-11-08 03:38:44 -0300

    Updated installer

--- install.sh
+++ install.sh
@@ -13,12 +13,19 @@ echo installing pulseaudio...
 install_pulse
 
 ##---Removing Packages---##
-sudo pacman --noconfirm -Rcs picom
+sudo pacman --noconfirm -Rcs picom palemoon-bin
 
 ##---Install Packages---##
 echo installing packages...
 sudo pacman --noconfirm -S yay firefox code kitty vlc neofetch libreoffice-fresh python-pywal rofi
-yay --answerclean a --answerdiff n -S cava-git ncspot-git balena-etcher bpytop-git cmatrix polybar tty-clock lightcord-git picom-tryone-git stacer-git
+yay --answerclean a --answerdiff n -S cava-git ncspot-git bpytop-git cmatrix polybar tty-clock lightcord-git picom-tryone-git stacer-git
+
+##---Install oh-my-zsh and plugins---##
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
 ##---Copy Dotfiles---##
 cp -r ~/dotfiles/kitty ~/.config/
@@ -26,3 +33,4 @@ cp -r ~/dotfiles/neofetch ~/.config/
 cp -r ~/dotfiles/polybar ~/.config/
 cp -r ~/dotfiles/picom.conf ~/.config/
 cp -r ~/dotfiles/wallpapers ~/
+cp -r ~/dotfiles/.zshrc ~/.zshrc
