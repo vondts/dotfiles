59d014281db483fa5eb9a963778db57f1ad58fec
Von <pedrodeibarlucea@gmail.com>
2020-11-19 00:25:14 -0300

    Update polybar conf

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 & disown
+polybar bar1 >>/tmp/polybar1.log 2>&1 & disown
+polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
 
 echo "Bars launched..."
