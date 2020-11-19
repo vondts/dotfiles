f929cd32794320e222bea0bff80ee9abd873cc17
vondts <pedrodeibarlucea@gmail.com>
2020-11-19 10:16:10 -0300

    Updated

--- install.sh
+++ install.sh
@@ -8,25 +8,24 @@
 echo updating system...
 sudo pacman --noconfirm -Syyu
 
+##---Removing Packages---##
+sudo pacman -Rcs picom palemoon-bin
+
 ##---Install Pulseaudio---##
 echo installing pulseaudio...
 install_pulse
 
-##---Removing Packages---##
-sudo pacman --noconfirm -Rcs picom palemoon-bin
-
 ##---Install Packages---##
 echo installing packages...
-sudo pacman --noconfirm -S yay firefox code kitty vlc neofetch libreoffice-fresh python-pywal rofi redshift discord python steam
+sudo pacman --noconfirm -S yay firefox code kitty neofetch libreoffice-fresh python-pywal rofi redshift discord python steam
 yay --answerclean a --answerdiff n -S cava-git ncspot-git bpytop-git polybar-git picom-tryone-git stacer-git plymouth
 
 ##---Install oh-my-zsh and plugins---##
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
-
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 ##---Install pywalfox---##
 pip install pywalfox
 pywalfox install
@@ -38,8 +37,3 @@ cp -r ~/dotfiles/neofetch ~/.config/
 cp -r ~/dotfiles/polybar ~/.config/
 cp -r ~/dotfiles/picom.conf ~/.config/
 cp -r ~/dotfiles/wallpapers ~/
-cp -r ~/dotfiles/.zshrc ~/.zshrc
-
-##---Config zsh---##
-
-kitty -e p10k configure
