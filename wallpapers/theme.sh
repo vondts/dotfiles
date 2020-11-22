d8b124a32f1c1d3d8cfc4f1f0fd7c07a32423443
vondts <pedrodeibarlucea@gmail.com>
2020-11-21 21:37:22 -0300

    Optimized wallpapers size

--- wallpapers/theme.sh
+++ wallpapers/theme.sh
@@ -3,13 +3,14 @@
 # Script for random selection of wallpapers 
 # for light and dark set by time.
 #
-
+WALL_DK=$(find "$~/wallpapers/dk/" -type f | sort -R | tail -1)
+WALL_LT=$(find "$~/wallpapers/lt/" -type f | sort -R | tail -1)
 TIME=$(date  +%H:%M)
 START="18:00"
 FINISH="08:00"
 
 if [[ "$TIME" > "$START" ]] || [[ "$TIME" < "$FINISH" ]]; then
-     wal -i ~/wallpapers/dk/ && pywalfox update
+     wal -i ~/wallpapers/dk/ && pywalfox update && feh --no-fehbg --bg-scale "$WALL_DK"
    else
-     wal -i ~/wallpapers/lt/ -l && pywalfox update
+     wal -i ~/wallpapers/lt/ -l && pywalfox update && feh --no-fehbg --bg-scale "$WALL_LT"
    fi
